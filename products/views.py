2a48e960ab0a9b0befa08cf180c730f5d5db372a
GurraT <gustafthomsson@hotmail.com>
2021-05-03 18:01:27 +0000

    added shoppingfunction

--- products/views.py
+++ products/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
+from .models import Product, Category
 
 # Create your views here.
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -21,7 +20,7 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sort_key = 'lower_name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
@@ -30,12 +29,11 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -45,9 +43,8 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
-    current_sorting = f'{sort}_{direction}'
 
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
